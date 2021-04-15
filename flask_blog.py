bcb85273d0bf0712eafb8d2642ae9a268a54876c
Meti A <metiade7@gmail.com>
2021-04-15 22:11:32 +0300

    working with flash messages

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash
 from forms import RegisterationForm, LoginForm;
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f38562b513d05c4a9e68ba32d2f7ca69'
@@ -29,7 +29,10 @@ def m():
 def register():
     form = RegisterationForm()
     if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
         return redirect(url_for('hello'))
+    if form.is_submitted() and not form.validate_on_submit():
+        flash(f'Invalid input', 'danger')
     return render_template('signup.html', title="Register", form = form)
 @app.route('/login')
 def login():
