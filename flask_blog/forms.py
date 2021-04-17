8998ec2489e6246ecc0847c18ed3195345028659
Meti A <metiade7@gmail.com>
2021-04-17 17:13:21 +0300

    Project structure update

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_blog.modles import User
+
+
 class RegisterationForm(FlaskForm):
     username = StringField('Username', 
                             validators=[DataRequired(), Length(min=4, max=10)
@@ -20,6 +23,16 @@ class RegisterationForm(FlaskForm):
                                     ])
     submit = SubmitField("signup")
     
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError("This username is already taken choose a different one please!")
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError("There is already an account with this email please change your email or login to your account")
 class LoginForm(FlaskForm):
     
     email = StringField("Email", validators=[DataRequired(), Email()])
